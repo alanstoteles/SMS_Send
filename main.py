4127154747f01656ef6efddcb06d8312da95cb1a
chalansillo <6160603-chalansillo@users.noreply.replit.com>
2023-10-29 19:28:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def main(service):
+  if service == 'twilio':
+    from twilio.rest import Client
+  
+    # Your Twilio account SID and Auth Token
+    ACCOUNT_SID = 'AC5b14dac2113b441171ad1af1f650be05'
+    AUTH_TOKEN = 'fbd993578367bf817b1176b897cfe052'
+  
+    # Create a client
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+  
+    # Send the SMS
+    message = client.messages.create(
+        body="Hello from Twilio!",  # Your message content
+        from_="+12512701257",  # Your Twilio phone number
+        to="+4917672753971"  # The recipient's phone number
+    )
+  
+    print(f"Message sent with SID: {message.sid}")
+  else:
+    pass
+    
+if __name__=='__main__':
+  #main('twilio')
+  main('ntfy')
